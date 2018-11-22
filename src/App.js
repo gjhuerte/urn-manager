afc31d7a6ef0c93e64559e84339a7b5e0d99fef0
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-22 09:49:59 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import reducers from './reducers';
+
+class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyC6nnVpSUU_z3YMMn38YxSFOlcu0guGuQU',
+            authDomain: 'manager-6226f.firebaseapp.com',
+            databaseURL: 'https://manager-6226f.firebaseio.com',
+            projectId: 'manager-6226f',
+            storageBucket: 'manager-6226f.appspot.com',
+            messagingSenderId: '950176402011'
+        });
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
