8bc4a87791b6e8b08f1f20a75ccb9a65ca5fb514
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-23 22:52:36 +0800

    Additional refactoring on controllers

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
     onEmailChange(text) {
@@ -27,7 +27,21 @@ class LoginForm extends Component {
                         {this.props.error}
                     </Text>
                 </View>
-            )
+            );
+        }
+
+        return (
+            <Button
+                onPress={this.onButtonPress.bind(this)}
+            >
+                Login
+            </Button>
+        );
+    }
+    
+    renderButton() {
+        if (this.props.loading) {
+            return <Spinner size="large" />;
         }
     }
 
@@ -56,11 +70,7 @@ class LoginForm extends Component {
                 {this.renderError()}
 
                 <CardSection>
-                    <Button
-                        onPress={this.onButtonPress.bind(this)}
-                    >
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
@@ -75,12 +85,10 @@ const styles = {
     },
 };
 
-const mapStateToProps = state => {
-    return {
-        email: state.auth.email,
-        password: state.auth.password,
-        error: state.auth.error,
-    };
+const mapStateToProps = ({ auth }) => {
+    const { email, password, error, loading } = auth;
+
+    return { email, password, error, loading };
 };
 
 export default connect(mapStateToProps, { 
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,6 +3,7 @@ import {
     PASSWORD_CHANGED,
     LOGIN_USER_SUCCESS,
     LOGIN_USER_FAIL,
+    LOGIN_USER,
 } from '../actions/types';
 
 const INITIAL_STATE = { 
@@ -10,6 +11,7 @@ const INITIAL_STATE = {
     password: '',
     user: null,
     error: '',
+    loading: false,
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -18,10 +20,12 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+        case LOGIN_USER:
+            return { ...state, loading: true, error: '' };
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload, error: '' };
+            return { ...state, user: action.payload, error: '', loading: false };
         case LOGIN_USER_FAIL:
-            return { ...state, error: 'Authentication Failed' };
+            return { ...state, error: 'Authentication Failed', loading: false };
         default:
             return state;
     }
