c625110aec6cfb1b3e3c2dc3859dca74e2098d94
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-23 12:11:41 +0800

    Use redux-thunk in the project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
+import firebase from '@firebase/app';
+import '@firebase/auth';
 import { 
     EMAIL_CHANGED, 
-    PASSWORD_CHANGED 
+    PASSWORD_CHANGED ,
+    LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -15,4 +18,13 @@ export const passwordChanged = (text) => {
         type: PASSWORD_CHANGED,
         payload: text
     };
+};
+
+export const loginUser = ({ email, password }) => {
+    return (dispatch) => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => {
+            dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+        });
+    }; 
 };
